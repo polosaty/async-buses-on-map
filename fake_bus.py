27997ce1e2c81784cc8472e62da581ab87e8e60c
Aksarin Mikhail <m.aksarin@gmail.com>
2021-07-22 00:15:52 +0500

    Add fake_bus

--- fake_bus.py
+++ fake_bus.py
@@ -1,13 +1,14 @@
 import contextlib
+from functools import partial
 from itertools import cycle
 from itertools import islice
 import json
 import logging
 import os
 import random
-from sys import stderr
-from typing import List, Tuple
+from typing import List
 
+import asyncclick as click
 import trio
 import trio_websocket
 from trio_websocket import open_websocket_url
@@ -15,32 +16,43 @@ from trio_websocket import open_websocket_url
 logger = logging.getLogger('fake_bus')
 
 
+def relaunch_on_disconnect(async_function):
+    async def wrapped(*args, **kwargs):
+        while True:
+            try:
+                logger.debug("%r starting", async_function.__name__)
+                await async_function(*args, **kwargs)
+            except trio.Cancelled:
+                break
+            except (trio_websocket.ConnectionClosed, Exception) as ex:
+                logger.error('%r %r', async_function.__name__, ex)
+                await trio.sleep(1)
+
+    return wrapped
+
+
 async def consume_and_send(url, messages_channel: trio.MemoryReceiveChannel):
-    try:
-        async with messages_channel:
-            async with open_websocket_url(url) as ws:
-                async for message in messages_channel:
-                    try:
-                        await ws.send_message(message)
-                    except trio.Cancelled:
-                        break
-
-    except OSError as ose:
-        logger.error('Connection attempt failed: %s', ose)
+    async with messages_channel:
+        await connect_and_send(url, messages_channel)
 
 
-async def run_bus(messages_channel: trio.MemorySendChannel, bus_id, route, skip_coordinates=0):
+@relaunch_on_disconnect
+async def connect_and_send(url, messages_channel: trio.MemoryReceiveChannel):
+    ws: trio_websocket.WebSocketConnection
+    async with open_websocket_url(url) as ws:
+        async for message in messages_channel:
+            await ws.send_message(message)
+
+
+async def run_bus(messages_channel: trio.MemorySendChannel, bus_id, route, skip_coordinates=0, refresh_timeout=1):
     async with messages_channel:
         coordinates = islice(cycle(route['coordinates']), skip_coordinates, None)
         bus_data = {"busId": bus_id, "route": route['name']}
         for coordinate in coordinates:
-            try:
-                message = json.dumps(dict(bus_data, lat=coordinate[0], lng=coordinate[1]))
-                await messages_channel.send(message)
-                await trio.sleep(1)
-            # await trio.sleep(0.1)
-            except trio.Cancelled:
-                break
+            message = json.dumps(dict(bus_data, lat=coordinate[0], lng=coordinate[1]))
+            await messages_channel.send(message)
+            await trio.sleep(refresh_timeout)
+
 
 def load_routes(directory_path='routes'):
     for filename in os.listdir(directory_path):
@@ -61,31 +73,56 @@ def make_uniq_bus_id(existing_ids, route_name):
     return bus_id
 
 
-def generate_bus_id(route_id, bus_index):
+def generate_bus_id(route_id, bus_index, prefix):
+    if prefix:
+        return f"{prefix}-{route_id}-{bus_index}"
     return f"{route_id}-{bus_index}"
 
 
-async def main():
-    url = os.getenv('SERVER_URL', 'ws://app:8080')
+@click.command()
+@click.option("--server", envvar='SERVER_URL', default='ws://app:8080', type=click.STRING,
+              help="адрес сервера")
+@click.option("--routes_number", envvar='ROUTES_NUMBER', default=1, type=click.INT,
+              help="количество маршрутов (не больше чем файлов в директории route)")
+@click.option("--buses_per_route", envvar='BUSES_PER_ROUTE', default=1, type=click.INT,
+              help="количество автобусов на каждом маршруте")
+@click.option("--websockets_number", envvar='WEBSOCKETS_NUMBER', default=1, type=click.INT,
+              help="количество открытых веб-сокетов")
+@click.option("--emulator_id", envvar='EMULATOR_ID', default='', type=click.STRING,
+              help="префикс к busId на случай запуска нескольких экземпляров имитатора")
+@click.option("--refresh_timeout", envvar='REFRESH_TIMEOUT', default=1, type=click.FLOAT,
+              help="задержка в обновлении координат сервера (сек.)")
+@click.option('-v', '--verbose', count=True,
+              help="настройка логирования")
+async def main(server, routes_number, buses_per_route, websockets_number, emulator_id, refresh_timeout, verbose):
+    logging.basicConfig(
+        level={
+            1: logging.WARNING,
+            2: logging.INFO,
+            3: logging.DEBUG,
+        }.get(verbose, os.getenv('LOG_LEVEL', logging.ERROR))
+    )
+
     async with trio.open_nursery() as nursery:
         send_channels: List[trio.MemorySendChannel] = []
-        for _ in range(10):
+        for _ in range(websockets_number):
             send_channel: trio.MemorySendChannel
             receive_channel: trio.MemoryReceiveChannel
             send_channel, receive_channel = trio.open_memory_channel(0)
-            nursery.start_soon(consume_and_send, url, receive_channel)
+            nursery.start_soon(consume_and_send, server, receive_channel)
             send_channels.append(send_channel)
 
-        for route in load_routes():
-            for bus_index in range(20000 // 500):
-                bus_id = generate_bus_id(route['name'], bus_index)
-                nursery.start_soon(run_bus,
-                                   random.choice(send_channels),
-                                   bus_id, route, random.randint(0, len(route['coordinates'])))
+        for route in islice(load_routes(), 0, routes_number):
+            for bus_index in range(buses_per_route):
+                bus_id = generate_bus_id(route['name'], bus_index, emulator_id)
+                nursery.start_soon(
+                    partial(run_bus,
+                            random.choice(send_channels),
+                            bus_id, route,
+                            skip_coordinates=random.randint(0, len(route['coordinates'])),
+                            refresh_timeout=refresh_timeout))
 
-            # break
 
 if __name__ == '__main__':
-    logging.basicConfig(level=os.getenv('LOG_LEVEL', logging.DEBUG))
     with contextlib.suppress(KeyboardInterrupt):
-        trio.run(main)
+        main(_anyio_backend="trio")
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
+import contextlib
+from dataclasses import asdict
+from dataclasses import dataclass
 from functools import partial
 import json
 import logging
 import os
 
+import asyncclick as click
 import trio
 from trio_websocket import ConnectionClosed
 from trio_websocket import serve_websocket
@@ -13,33 +17,100 @@ logger = logging.getLogger('server')
 BUSES = {}
 
 
+class WebsocketMessageParseError(Exception):
+    def __init__(self, error, *args):
+        self.error = error
+        super().__init__(*args)
+
+
+@dataclass
+class Bus:
+    lat: float
+    lng: float
+    busId: str
+    route: str
+
+
+@dataclass
+class WindowBounds:
+    south_lat: float = 0
+    north_lat: float = -1
+    west_lng: float = 0
+    east_lng: float = -1
+
+    def is_inside(self, bus: Bus):
+        try:
+            return (
+                self.south_lat <= bus.lat <= self.north_lat
+                and self.west_lng <= bus.lng <= self.east_lng
+            )
+        except Exception as ex:
+            logger.exception('WindowBounds.is_inside %r', ex)
+
+        return False
+
+    def is_real(self):
+        return self.south_lat < self.north_lat and self.west_lng < self.east_lng
+
+    def update(self, south_lat, north_lat, west_lng, east_lng):
+        self.south_lat = south_lat
+        self.north_lat = north_lat
+        self.west_lng = west_lng
+        self.east_lng = east_lng
+
+
 async def serve_outcome(request: WebSocketRequest):
     ws: WebSocketConnection = await request.accept()
+    bounds = WindowBounds()
+    with contextlib.suppress(ConnectionClosed):
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(listen_browser, ws, bounds)
+            nursery.start_soon(talk_to_browser, ws, bounds)
 
+
+async def talk_to_browser(ws: WebSocketConnection, bounds: WindowBounds):
     while True:
-        try:
-            # message = await ws.get_message()
-            # message = json.dumps({
-            #     "msgType": "Buses",
-            #     "buses": [
-            #         {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
-            #         {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
-            #     ]
-            # })
-
-            message = json.dumps({
+        if bounds.is_real():
+            message_data = {
                 "msgType": "Buses",
                 "buses": [
-                    {"busId": bus_id,
-                     "lat": bus_data['lat'],
-                     "lng": bus_data['lng'],
-                     "route": bus_data['route']}
-                    for bus_id, bus_data in BUSES.items()
-                    if bus_data.get('lat') and bus_data.get('lng')
+                    asdict(bus)
+                    for bus in BUSES.values()
+                    if (bounds.is_inside(bus))
                 ]
-            })
+            }
+            logger.debug('%r buses inside bounds', len(message_data['buses']))
+            message = json.dumps(message_data)
             await ws.send_message(message)
-            await trio.sleep(1)
+        await trio.sleep(1)
+
+
+async def listen_browser(ws: WebSocketConnection, bounds: WindowBounds):
+    """Update bounds by newBounds from ws."""
+    while True:
+        try:
+            message = json.loads(await ws.get_message())
+
+            if not message.get("msgType"):
+                logger.warning('msgType not in message: %r', message)
+                raise WebsocketMessageParseError("Requires msgType specified")
+
+            if message["msgType"] == "newBounds":
+                # newBounds message example:
+                #  {"msgType":"newBounds",
+                #   "data":{"south_lat":55.7256116937982,"north_lat":55.77435239625299,
+                #           "west_lng":37.54019737243653,"east_lng":37.65984535217286}}
+                bounds.update(**message['data'])
+
+            logger.debug('%r', message)
+
+        except json.JSONDecodeError as ex:
+            logger.warning('%r', ex)
+            await ws.send_message(json.dumps({"errors": ['Requires valid JSON'], "msgType": "Errors"}))
+
+        except WebsocketMessageParseError as ex:
+            await ws.send_message(json.dumps({"errors": [ex.error], "msgType": "Errors"}))
+
         except ConnectionClosed:
             break
 
@@ -50,23 +121,48 @@ async def serve_income(request: WebSocketRequest):
     while True:
         try:
             bus_data = json.loads(await ws.get_message())
-            bus_id = bus_data['busId']
-            BUSES[bus_id] = bus_data
-            logger.debug('%r',
-                         {"busId": bus_data['busId'],
-                          "lat": bus_data['lat'],
-                          "lng": bus_data['lng'],
-                          "route": bus_data['route']})
+
+            try:
+                bus = Bus(**bus_data)
+                assert isinstance(bus.lng, (int, float)) and isinstance(bus.lat, (int, float))
+            except (TypeError, AssertionError):
+                raise WebsocketMessageParseError('Requires busId: str, lat: float, lng: float and route: str specified')
+
+            BUSES[bus.busId] = bus
+            logger.debug('%r', asdict(bus))
+
+        except json.JSONDecodeError as ex:
+            logger.warning('%r', ex)
+            await ws.send_message(json.dumps({"errors": ['Requires valid JSON'], "msgType": "Errors"}))
+
+        except WebsocketMessageParseError as ex:
+            await ws.send_message(json.dumps({"errors": [ex.error], "msgType": "Errors"}))
+
         except ConnectionClosed:
             break
 
 
-async def main():
+@click.command()
+@click.option("--browser_port", envvar='BROWSER_PORT', default=8000, type=click.INT,
+              help="порт для браузера")
+@click.option("--bus_port", envvar='BUS_PORT', default=8080, type=click.INT,
+              help="порт для имитатора автобусов")
+@click.option('-v', '--verbose', count=True,
+              help="настройка логирования")
+async def main(browser_port, bus_port, verbose):
+    logging.basicConfig(
+        level={
+            1: logging.WARNING,
+            2: logging.INFO,
+            3: logging.DEBUG,
+        }.get(verbose, os.getenv('LOG_LEVEL', logging.ERROR))
+    )
+
     async with trio.open_nursery() as nursery:
-        nursery.start_soon(partial(serve_websocket, serve_outcome, '0.0.0.0', 8000, ssl_context=None))
-        nursery.start_soon(partial(serve_websocket, serve_income, '0.0.0.0', 8080, ssl_context=None))
+        nursery.start_soon(partial(serve_websocket, serve_outcome, '0.0.0.0', browser_port, ssl_context=None))
+        nursery.start_soon(partial(serve_websocket, serve_income, '0.0.0.0', bus_port, ssl_context=None))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=os.getenv('LOG_LEVEL', logging.DEBUG))
-    trio.run(main)
+    with contextlib.suppress(KeyboardInterrupt):
+        main(_anyio_backend="trio")
